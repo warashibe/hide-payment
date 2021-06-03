ff8de33a03397f0e065de0ec86cf92e9a095b145
Tomoya Nagasawa <ocrybit@gmail.com>
2021-06-04 00:07:39 +0900

    v2 with swap

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,5 @@
 require("@nomiclabs/hardhat-waffle")
-
+require("@nomiclabs/hardhat-ethers")
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
 task("accounts", "Prints the list of accounts", async () => {
@@ -17,5 +17,5 @@ task("accounts", "Prints the list of accounts", async () => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.6.6",
+  solidity: "0.6.12",
 }
